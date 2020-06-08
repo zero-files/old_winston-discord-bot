9457549f580952701b00b0a1cf6615666ac4f23f
Enzo Diaz <enzodiazdev@gmail.com>
2020-06-08 16:08:42 -0300

    autocommander add

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,4 +1,7 @@
-import {Client} from "discord.js"
+import {Client, Message} from "discord.js"
+import {readdirSync} from "fs"
+import path from "path"
+import Command from "../utils/Command"
 
 /**
  * @typedef Bot_Options
@@ -7,6 +10,7 @@ import {Client} from "discord.js"
  * @property {string} on_ready_log Mensaje de consola para cuando el bot esté listo. 
  * @property {string} commands_folder Carpeta contenedora de comandos.
  * @property {string} help_command Trigger para el comando de ayuda, por defecto "help"
+ * @property {string} about_command Trigger para el comando de acerca de, por defecto "about"
  *
  */
 
@@ -42,6 +46,9 @@ class Bot extends Client {
         /** Comando de ayuda */
         this.help_command = options.help_command ? options.help_command.toLowerCase().trim() : "help"
 
+        /** Comando de acerca de */
+        this.about_command = options.about_command ? options.about_command.toLowerCase().trim() : "about"
+
         /** Ubicación de los comandos */
         this.commands_folder = options.commands_folder || ""
 
@@ -52,11 +59,87 @@ class Bot extends Client {
     }
 
     /**
-     * Prepara al bot para ser iniciado
+     * Función previa al encendido del bot.
+     * 
+     * Debe ser sobreescrita.
+     * 
+     * Puede retornar una Promise.
+     */
+    prestart(){
+        return true
+    }
+
+    /**
+     * Enciende el bot
      * @param {string} token Api Token del Bot
      */
     async start(token){
+        await this.prestart()
+        await this.commander()
+
         this.login(token)
+    }
+
+    /**
+     * Se encarga de manejar y ejecutar los comandos
+     * @param {Message} message El objeto Message de discord.js
+     */
+    commandHandler(message){
+        let messageWithoutPrefix = message.content.split(" ")[0] === this.prefix ?
+            message.content.split(" ").slice(1).join(" ").toLowerCase().trim() :
+            message.content.slice(this.prefix.length).toLowerCase().trim()
+    
+        let userCommand = messageWithoutPrefix.split(" ")[0].trim()
+        let params = messageWithoutPrefix.split(" ").slice(1).map(parameter => parameter.trim()).filter(parameter => parameter !== "")
+        
+        if(this.commandList.indexOf(userCommand) === -1) return
+
+        let command = this.commands.find(command => command.trigger === userCommand)
+
+        if(!command) return
+
+        try { command.exe()(message, ...params) }
+        catch (e) {
+            console.log(e)
+            return message.channel.send(`Ha ocurrido un error en este comando. Contacta con el desarrollador: ${command.author}`)
+        }
+    }
+
+    /**
+     * Importa todos los comandos
+     * @private
+     */
+    commander(){
+        return new Promise(resolve => {
+            let commandFiles = readdirSync(this.commands_folder)
+        
+            for(let i = 0; i < commandFiles.length; i++){
+                let file = commandFiles[i]
+                if(file.startsWith("_") || !file.endsWith(".js")) continue
+                
+                let module = require(path.join(this.commands_folder, file))
+                let command = module.default || module
+
+                if(!command instanceof Command) continue
+                if(!command.trigger) continue
+    
+                this.commands.push(command)
+                this.commandList.push(command.trigger)
+            }
+
+            resolve()
+        })
+    }
+
+    /**
+     * Agrega comandos al Bot
+     * @param {Command} command
+     */
+    addCommand(command){
+        if(command instanceof Command && command.trigger){
+            this.commands.push(command)
+            this.commandList.push(command.trigger)
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 if(process.env.NODE_ENV == "development") require("dotenv").config()
 import Bot from "./bot/bot"
+import path from "path"
 
 const bot = new Bot({
     on_ready_log: "Beepboop o/",
     prefix: process.env.PREFIX || "$",
-    name: "Winston"
+    name: "Winston",
+    commands_folder: path.resolve("./src/commands/")
 })
 
 async function main(){
-    bot.start(process.env.BOTKEY);
+    bot.on("message", message => {
+        if(!message.content.startsWith(bot.prefix) || message.author.bot) return
+        bot.commandHandler(message)
+    })
+
+    bot.start(process.env.BOTKEY)
 }
 
 main()
