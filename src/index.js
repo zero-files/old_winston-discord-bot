5dfac45973c355e3afb17b02da01b277e4972ea7
Enzo Diaz <enzodiazdev@gmail.com>
2020-06-08 19:21:48 -0300

    base de datos eliminada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 if(process.env.NODE_ENV == "development") require("dotenv").config()
 import Bot from "./bot/bot"
-import path from "path" 
-import {get, put} from "./database/database"
+import path from "path"
 
 Array.prototype.remove = function(element){ let i = this.indexOf(element); if(i != -1) this.splice(i, 1) }
 
@@ -12,39 +11,13 @@ const bot = new Bot({
     commands_folder: path.resolve("./src/commands/")
 })
 
-var waitingList = []
-
 async function main(){
     bot.prestart = () => new Promise((resolve, reject) => {
         bot.addCommand(bot.createHelpCommand())
         resolve()
     })
 
-    var presets = await get.presets()
-
-    bot.on("message", message => {
-        if(!presets) presets = await get.presets()
-
-        /** @type {{lang:string, prefix:string}} */
-        let server = presets[message.guild.id] || undefined
-        if(!server) {
-            if(waitingList.indexOf(message.guild.id) != -1) return
-            else {
-                waitingList.push(message.guild.id)
-                await put.serverData({
-                    modules: { PLACEHOLDER:"PLACEHOLDER" },
-                    config: {
-                        lang: "es",
-                        prefix: bot.prefix
-                    },
-                    data: { name: message.guild.name }
-                })
-                presets = await get.presets()
-                waitingList.remove(message.guild.id)
-                return
-            }
-        }
-
+    bot.on("message", async message => {
         if(!message.content.startsWith(bot.prefix) || message.author.bot) return
         bot.commandHandler(message)
     })
